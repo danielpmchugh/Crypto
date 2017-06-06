16ea94006e908bd9f9f9b764dbc7b21dd8e1efa8
gspadz <danielpmchugh@gmail.com>
2017-06-05 21:58:56 -0400

    Add project files.

---
+++ HashPlayground/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HashPlayground
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please enter the string you wish to hash");
+            }
+            var input = args[1];
+            var output = MySha1(input);
+            Console.WriteLine(output);
+        }
+
+        static string MySha1(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
