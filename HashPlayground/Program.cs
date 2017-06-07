f05c738e315ae451af8a79eb048bbfbb5da1e736
gspadz <danielpmchugh@gmail.com>
2017-06-06 21:25:18 -0400

    Added msft version of sha1 to compare against

--- HashPlayground/Program.cs
+++ HashPlayground/Program.cs
@@ -1,23 +1,52 @@
 ﻿namespace HashPlayground
 {
     using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
 
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Please enter the string you wish to hash");
-            }
-            var input = args[1];
-            var output = MySha1(input);
-            Console.WriteLine(output);
+
+            var length = (int)new Random().Next() % 1000000;
+            var input = RandomString(length);
+            var myOutput = MySha1(input);
+            var correctOutput = MsftSha1(input);
+            Console.WriteLine($"Input (first 50)(actual {length}: {input.Substring(0, 50 < length ? 50 : length-1)}");
+            Console.WriteLine($"Match: {myOutput == correctOutput}");
+            Console.WriteLine($"My Sha1: {myOutput}");
+            Console.WriteLine($"Correct Sha1 {correctOutput}");
         }
 
-        static string MySha1(string input)
+        private static string MySha1(string input)
         {
-            throw new NotImplementedException();
+            return string.Empty;
+        }
+
+        private static string MsftSha1(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+        
+        private static string RandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
